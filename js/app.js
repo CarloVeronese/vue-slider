48cffa6fc66b1600cef29150bf40e5474a55afe1
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-24 14:05:28 +0200

    Add - next and prev functions. fixed image active

--- js/app.js
+++ js/app.js
@@ -31,7 +31,18 @@ createApp({
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
-            ]
+            ],
+            currentIndex: 0
+        }
+    },
+    methods: {
+        nextImage(){
+            this.currentIndex ++;
+            if(this.currentIndex == this.slides.length) this.currentIndex = 0;
+        },
+        prevImage(){
+            this.currentIndex --;
+            if(this.currentIndex < 0) this.currentIndex = this.slides.length - 1;
         }
     }
 }).mount('#app')
